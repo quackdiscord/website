8e04c69f14d7e1a2538e502c7b11aa70827cd4d2
Kyle Dickey <41174949+dickeyy@users.noreply.github.com>
2024-02-16 04:56:01 -0700

    fix pic

--- comps/hero/discordElement.tsx
+++ comps/hero/discordElement.tsx
@@ -3,7 +3,6 @@ import { useEffect, useState } from "react";
 import { FaHashtag, FaPlusCircle } from "react-icons/fa";
 
 export default function DiscordElement() {
-
     return (
         <div className="bg-[#323337] w-full h-full p-4 rounded-lg justify-between flex flex-col">
             <div className="border-b border-zinc-600 flex flex-row pb-4 items-center">
@@ -13,16 +12,14 @@ export default function DiscordElement() {
 
             <div>
                 <div className="">
-                    <Image src={`/images/png/seeds-website-cases-showcase.png`} alt="Discord Cases Image" width={500} height={500} />
+                    <Image src={`/images/png/quack-discord.png`} alt="Discord Image" width={500} height={500} />
                 </div>
 
                 <div className="bg-[#373a3f] rounded-lg mt-4 p-3 flex-row flex text-zinc-500 items-center">
                     <FaPlusCircle className="text-xl mr-2" />
-                    <p>
-                        Message #general
-                    </p>
+                    <p>Message #general</p>
                 </div>
             </div>
         </div>
-    )
+    );
 }
