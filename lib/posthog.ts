ebb5ab35f193a3efcb6310b70ed7e758bf4010b6
Kyle Dickey <k@kyle.so>
2024-07-03 03:39:17 -0600

    add plausible

--- lib/posthog.ts
+++
@@ -1,5 +0,0 @@
-import posthog, { PostHog } from "posthog-js";
-
-export const hog: void | PostHog = posthog.init("phc_XGVCgPe5Tc6VEHP2qM71TM3VG3kanvO0OWU8SgZwKV0", {
-    api_host: "https://app.posthog.com"
-});
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,8 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { hog } from "../lib/posthog";
 import { SessionProvider } from "next-auth/react";
 
 export default function App({ Component, pageProps }: AppProps) {
-    if (hog) hog;
-
     return (
         <SessionProvider session={pageProps.session}>
             <Component {...pageProps} />
